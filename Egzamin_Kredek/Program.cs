3528c69b729db7afb89c89fb335a85d08b33c553
Yetisio <jakub.piekarek@wp.pl>
2022-01-26 19:23:40 +0100

    26.01

---
+++ Egzamin_Kredek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Egzamin_Kredek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 6; i++)
+            {
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
